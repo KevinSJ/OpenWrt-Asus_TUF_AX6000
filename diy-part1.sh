a47ae249dc22697501312f1bf54d4ef3db50b260
Kevin Jiang <KevinSJ@users.noreply.github.com>
2025-05-17 16:18:11 +1200

    Adding patches

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# 20230710: Using latest tag, could be rc
+#version_tag="$(git tag -l|grep -v 'rc'|tail -1)"
+#git checkout "$version_tag"
+#llvm_tag=$(echo $version_tag|tr -d 'v')
+
+# 20240824
+version_tag="$(git tag -l|grep -v 'rc'|tail -1)"
+git checkout "$version_tag"
+llvm_tag=$(echo $version_tag|tr -d 'v')
+
+#
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git argontheme https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
+#echo 'src-git immortalwrt https://github.com/immortalwrt/packages' >>feeds.conf.default
+wget -O - https://downloads.openwrt.org/releases/"$llvm_tag"/targets/mediatek/filogic/llvm-bpf-18.1.7.Linux-x86_64.tar.xz | tar -xvaf -
+
+ln -sf "$(realpath llvm-bpf-18.1.7.Linux-x86_64)" llvm-bpf
+
+rm $GITHUB_WORKSPACE/$PATCH_DIR/0006-ARM-Cortex-A9-build-the-userspace-with-Thumb-2-instr.patch
+rm $GITHUB_WORKSPACE/$PATCH_DIR/0008-Add-divblock-an-extremely-simple-ad-blocker.patch
+
+# Apply patches
+
+#git am $GITHUB_WORKSPACE/$PATCH_DIR/*.patch --3way || (git checkout --theirs . && git add . && git am --continue)
+# skip patch if failed.
+git am $GITHUB_WORKSPACE/$PATCH_DIR/*.patch --3way || git am --skip
+
+
